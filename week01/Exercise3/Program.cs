3ad880a8ce32d278759b042bdf8ab07f7e1801cf
israelessien48-art <israelessien48@gmail.com>
2025-10-25 18:21:38 +0100

    Completed C# Programming Exercise 3: Loops

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Create a random number generator
+        Random randomGenerator = new Random();
+        string playAgain = "yes";
+
+        // Loop the entire game as long as user says yes
+        while (playAgain.ToLower() == "yes")
+        {
+            // Generate a random magic number between 1 and 100
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;  // initialize guess to something that isn't the magic number
+            int guessCount = 0; // track number of guesses
+
+            Console.WriteLine("Welcome to the Guess My Number game!");
+            Console.WriteLine("I have chosen a number between 1 and 100.");
+
+            // Loop until the user guesses correctly
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            // Ask if the user wants to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+            Console.WriteLine(); // blank line for spacing
+        }
+
+        Console.WriteLine("Thanks for playing! Goodbye.");
     }
 }
