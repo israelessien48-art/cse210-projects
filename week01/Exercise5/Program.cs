2454c56d93578a2e6d492880a7e5cc181bf77e8f
israelessien48-art <israelessien48@gmail.com>
2025-10-25 18:39:57 +0100

    git add . git commit -m "Completed C# Programming Exercise 5: Functions" git push

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Call each function in order
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(favoriteNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Function 1: Displays a welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function 2: Prompts for and returns the user's name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // Function 3: Prompts for and returns the user's favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    // Function 4: Accepts an integer and returns it squared
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    // Function 5: Displays the user's name and squared number
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
