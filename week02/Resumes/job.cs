fe0d9634e003eb98a2bba5644ec861eb46b0b78e
israelessien48-art <israelessien48@gmail.com>
2025-10-29 21:22:12 +0100

    update

---
+++ week02/Resumes/job.cs
@@ -0,0 +1,17 @@
+// Job.cs
+using System;
+
+public class Job
+{
+    // Member variables
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    // Method to display job details
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
