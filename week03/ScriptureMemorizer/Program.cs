30714da9f3e7ba11bdb08e7790b3fda16f2c6c39
israelessien48-art <israelessien48@gmail.com>
2025-10-31 14:20:57 +0100

    update

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -15,7 +15,7 @@ class Program
         {
             (new Reference("John", 3, 16), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."),
             (new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight."),
-            (new Reference("Philippians", 4, 13), "I can do all things through Christ which strengtheneth me.")
+            (new Reference("Philippians", 4, 13), "I can do all things through Christ which strengthens me.")
         };
 
         Random random = new Random();
